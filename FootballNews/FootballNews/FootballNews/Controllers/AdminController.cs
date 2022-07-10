5a0cb26d2e6c747f9fe75391f00cefbb2c429f31
Nguyen Tien Dat (K15 HL) <datnthe153704@fpt.edu.vn>
2022-07-10 20:12:46 +0700

    up add user admin

--- FootballNews/FootballNews/FootballNews/Controllers/AdminController.cs
+++ FootballNews/FootballNews/FootballNews/Controllers/AdminController.cs
@@ -57,6 +57,7 @@ namespace FootballNews.Controllers
             }
             if (userManager.GetUserByName(Username) != null || userManager.GetUserByEmail(Email) != null)
             {
+                ViewData["Error"] = false;
                 return ManageUser();
             }
             else
